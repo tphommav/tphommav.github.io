cd3f1ee56266f5ed8618316fc91d636da2ce5542
tphommav <tphommav@gmail.com>
2016-04-23 21:25:14 -0700

    ic2

--- js/index.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $('.js-nav a, .js-connect').click(function(e) {
-    e.preventDefault();
-    $('body, html').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
-    }, 750);
-  });
-});
